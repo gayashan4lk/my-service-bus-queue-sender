d51802694cc0136b4d6db33788b45dc708834347
gayashan4lk <gayashan4lk@gmail.com>
2024-08-21 14:41:32 +0530

    add settings.json to get config values

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("settings.json", optional:false, reloadOnChange: true)
+    .Build();
+
+var SERVICE_BUS_NAMESPACE_CONNECTION_STRING = configuration["AppSettings:SERVICE_BUS_NAMESPACE_CONNECTION_STRING"];
+var SERVICE_BUS_QUEUE_NAME = configuration["AppSettings:SERVICE_BUS_QUEUE_NAME"];
+
+Console.WriteLine($"SERVICE_BUS_NAMESPACE_CONNECTION_STRING:{SERVICE_BUS_NAMESPACE_CONNECTION_STRING}");
+Console.WriteLine($"SERVICE_BUS_QUEUE_NAME:{SERVICE_BUS_QUEUE_NAME}");
