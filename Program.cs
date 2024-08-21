b78a1656413f75e12e30e07bb7f4011fcfd6be65
gayashan4lk <gayashan4lk@gmail.com>
2024-08-21 14:55:55 +0530

    send messages to the service queue

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
 
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -8,5 +9,37 @@ var configuration = new ConfigurationBuilder()
 var SERVICE_BUS_NAMESPACE_CONNECTION_STRING = configuration["AppSettings:SERVICE_BUS_NAMESPACE_CONNECTION_STRING"];
 var SERVICE_BUS_QUEUE_NAME = configuration["AppSettings:SERVICE_BUS_QUEUE_NAME"];
 
+var numOfMessages = 100;
+
 Console.WriteLine($"SERVICE_BUS_NAMESPACE_CONNECTION_STRING:{SERVICE_BUS_NAMESPACE_CONNECTION_STRING}");
 Console.WriteLine($"SERVICE_BUS_QUEUE_NAME:{SERVICE_BUS_QUEUE_NAME}");
+
+var client = new ServiceBusClient(
+    SERVICE_BUS_NAMESPACE_CONNECTION_STRING, 
+    new ServiceBusClientOptions()
+    {
+        TransportType = ServiceBusTransportType.AmqpWebSockets
+    });
+
+var sender = client.CreateSender(SERVICE_BUS_QUEUE_NAME);
+
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+for (int i = 1; i < numOfMessages; i++)
+{
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Gaya service bus message {i}")))
+    {
+        throw new Exception($"The message {i} is too large to fit in the batch.");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
